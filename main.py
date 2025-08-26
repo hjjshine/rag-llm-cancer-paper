cd1fa5f0fc5185b11c9bc5428d4511befbe69ff6
yutaro-tanaka-yt2705 <yt2705@columbia.edu>
2025-08-26 10:31:28 -0400

    add in batch processing for LLM and RAG-LLM

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
 import subprocess
 from llm.run_LLM import main as run_llm_main
+from llm.run_LLM_batch import main as run_llm_batch_main
 from llm.run_RAGLLM import main as run_ragllm_main
+from llm.run_RAGLLM_batch import main as run_ragllm_batch_main
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', choices=['llm', 'rag-llm'], required=True)
+    parser.add_argument('--mode', choices=['llm', 'llm-batch', 'rag-llm', 'rag-llm-batch'], required=True)
     parser.add_argument('--csv_path', type=str, required=True, help="Path to input query CSV file with a 'prompt' column") 
     parser.add_argument('--model_type', choices=['mistral','mistral-7b','gpt','gpt_reasoning'], type=str, required=True, help="Type of model")
     parser.add_argument('--model_api', type=str, help="Model API name")
@@ -23,16 +25,24 @@ def parse_args():
 def main():
     args = parse_args()
     
-    if args.mode == "rag-llm": 
+    #check
+    if args.mode in ["rag-llm", "rag-llm-batch"]:
         if not args.context_chunks:
             raise ValueError("`--context_chunks` is required when mode is 'rag-llm'")
         if not args.model_api:
-            raise ValueError("`--model_api` is required when mode is 'rag-llm'")
+            raise ValueError("`--model_api` is required when mode is 'rag-llm'")        
+    if args.mode in ['llm-batch', 'rag-llm-batch'] and args.model_type not in ['gpt', 'gpt_reasoning']:
+        raise ValueError("Batch mode is only supported for 'gpt' and 'gpt_reasoning' models")
         
+    #run selected mode
     if args.mode == 'llm':
         run_llm_main(args)
+    elif args.mode == 'llm-batch':
+        run_llm_batch_main(args)
     elif args.mode == 'rag-llm':
         run_ragllm_main(args)
+    elif args.mode == 'rag-llm-batch':
+        run_ragllm_batch_main(args)
 
 if __name__ == '__main__':
     main()
